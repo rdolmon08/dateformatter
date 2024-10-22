ab9b466d793489cda9a6366f30d56e85120e4299
Rebecca D. <rdolmon08@gmail.com>
2024-10-22 08:10:13 -0500

    adding event listeners js

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
+const minutes = date.getMinutes();
 
-const minutes = date.getMinutes(); 
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+
+  }
+});
