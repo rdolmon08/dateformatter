40ca6964bc6f836f66b6f5104e3b01acfd6a7802
Rebecca D. <rdolmon08@gmail.com>
2024-10-23 14:03:19 -0500

    final js

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ dateOptionsSelectElement.addEventListener("change", () => {
         .reverse()
         .join("-");
       break;
-
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    
   }
 });
